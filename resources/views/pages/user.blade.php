27ddb72c3a3985bcf8dd7704c2640f67eba6e714
finn <finnnxptrix@gmail.com>
2025-07-16 14:46:32 +0700

    push

---
+++ resources/views/pages/user.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Users')
+
+@section('content_header')
+    <h1>Daftar Pengguna</h1>
+@endsection
+
+@section('content')
+    <p>Di sini nanti tampil daftar user ya sayang 🥰</p>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@endsection
+
+@section('content')
+    <p>Hai sayang, ini tampilan AdminLTE kamu 🥰</p>
+@endsection
